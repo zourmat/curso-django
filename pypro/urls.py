75385616a35e723c82ed8f943319ae635cb1c8cc
zourmat <matheusmpz@hotmail.com>
2022-05-03 00:29:55 -0300

    Configuração startapp base

--- pypro/urls.py
+++ pypro/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('y', include('pypro.base.urls'))
+    path('', include('pypro.base.urls'))
 ]
