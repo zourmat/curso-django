84be058e66d617afd968178cd3b56ef77bd9db46
zourmat <matheusmpz@hotmail.com>
2022-05-05 20:41:06 -0300

    Configuração para segurança e contribuição - DEBUG e contrib

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-olg*e**(27z##33cc@w%3z1zwe=td2(!2f-2+jamp$^4y9npk*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DUBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
