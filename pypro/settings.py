0b391af99c34688d84905ea1d3a7b5a5a61b5f13
zourmat <matheusmpz@hotmail.com>
2022-05-10 13:55:03 -0300

    Configurado bancos de dados

--- pypro/settings.py
+++ pypro/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 default_db_url = 'sqlite3:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 DATABASES = {
-    'default': config('DATABASE_URL', defaul=default_db_url, cast=parse_database)
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 # Password validation
