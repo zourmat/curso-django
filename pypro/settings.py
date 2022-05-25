496fd41a77e0b0553a0fb1e00d51ab27f9fc9237
zourmat <matheusmpz@hotmail.com>
2022-05-25 01:02:26 -0300

    Configurada lib Collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base'
 ]
@@ -129,6 +130,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # diretório onde serão co
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLE = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # STORAGE CONFIGURATION IN S3 AWS
@@ -144,6 +147,8 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_CUSTOM_DOMAIN = None
     COLLECTFAST_ENABLED = True
     AWS_DEFAULT_ACL = 'private'
+
+    COLLECTFAST_ENABLE = True
 
     # STATIC ASSETS
     # ====================================================================
