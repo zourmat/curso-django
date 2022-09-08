303836d8a030abe0c2fc76e4c0b2d385c0c4fd6d
zourmat <matheusmpz@hotmail.com>
2022-09-08 15:32:49 -0300

    Integrações simples

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,22 @@
+import pytest
+from django.urls import reverse
+
+# Create your tests here.
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('umvideo',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Vídeo Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<h1>Vídeo Aperitivo: Motivação</h1>')
