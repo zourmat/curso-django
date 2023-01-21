2265ca53a10abe9ecdaed6f5a55895d933be09fb
zourmat <matheusmpz@hotmail.com>
2023-01-21 17:56:31 -0300

    Processador de contexto

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from pypro.modulos import facade
 
 
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {'MODULOS': facade.listar_modulos_ordenados()})
