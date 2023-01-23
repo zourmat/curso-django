930de3d2c2e522f41d4e8f11b96a9bc7d1687b16
zourmat <matheusmpz@hotmail.com>
2023-01-23 12:46:50 -0300

    Acesso a relação com atributo_set

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -11,3 +11,7 @@ def listar_modulos_ordenados() -> list[Modulo]:
 
 def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
+
+
+def listar_aulas_modulo_ordenadas(modulo: Modulo):
+    return list(modulo.aula_set.order_by('order').all())
