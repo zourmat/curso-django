953c1d299dbe35d62a4e530619d2d4b4faf822cb
zourmat <matheusmpz@hotmail.com>
2023-01-26 22:24:35 -0300

    Revertendo migrações

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 class Turma(models.Model):
@@ -7,4 +6,3 @@ class Turma(models.Model):
     slug = models.CharField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
-    matriculas = models.ManyToManyField(get_user_model())
